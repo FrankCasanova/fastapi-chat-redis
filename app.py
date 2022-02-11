d7bc08c0fab27bbe5d5f7424e3ff0c82f0671447
frankcasanova <frankcasanova.info@gmail.com>
2022-02-11 16:41:58 +0100

    mock chat app finished

--- app.py
+++ app.py
@@ -5,16 +5,75 @@ from fastapi import FastAPI, WebSocket
 from pydantic import BaseModel
 from starlette.websockets import WebSocketDisconnect
 
-app = FastAPI
 
-#You must run a docker container with Redis image.
-broadcast = Broadcast('redis:localhost:6379')
-#This will be the name of the channel to publish
-#and subscribe to messages. (You could have more thant one)
+# You must run a docker container with Redis image.
+broadcast = Broadcast("redis://localhost:6379")
+# This will be the name of the channel to publish
+# and subscribe to messages. (You could have more thant one)
 CHANNEL = 'CHAT'
 
-async def receive_messages(
+app = FastAPI()
 
 
+class MessageEvent(BaseModel):
+    username: str
+    message: str
 
 
+async def receive_message(websocket: WebSocket,
+                          username: str):
+    """
+    Subscribe the broadcast channel and
+    waits for messages called event.
+    the parse_raw method of Pydantic allow to parse
+    JSON string into an object in one operation
+    """
+    async with broadcast.subscribe(channel=CHANNEL) as subscriber:
+        async for event in subscriber:
+            message_event = MessageEvent.parse_raw(event.message)
+            # discard user's own messages
+            if message_event.username != username:
+                await websocket.send_json(message_event.dict())
+
+
+async def send_message(websocket: WebSocket,
+                       username: str):
+    """
+    Waits for a new data in the socket, structures it
+    into a MessageEvent object and then publishes it.
+    """
+    data = await websocket.receive_text()
+    event = MessageEvent(username=username, message=data)
+    await broadcast.publish(channel=CHANNEL, message=event.json())
+
+
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket, username: str = "Anonymous"):
+    await websocket.accept()
+    try:
+        while True:
+            receive_message_task = asyncio.create_task(
+                receive_message(websocket, username)
+            )
+            send_message_task = asyncio.create_task(
+                send_message(websocket, username))
+            done, pending = await asyncio.wait(
+                {receive_message_task, send_message_task},
+                return_when=asyncio.FIRST_COMPLETED,
+            )
+            for task in pending:
+                task.cancel()
+            for task in done:
+                task.result()
+    except WebSocketDisconnect:
+        await websocket.close()
+
+
+@app.on_event('startup')
+async def startup():
+    await broadcast.connect()
+
+
+@app.on_event('shutdown')
+async def shutdown():
+    await broadcast.disconnect()
